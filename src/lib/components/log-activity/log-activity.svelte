0ad07428cc2f174917b122754d886a0bec4bfd71
jinwood <jinw@protonmail.com>
2024-10-27 18:00:08 +0000

    Add a slider for feedback component

--- src/lib/components/log-activity/log-activity.svelte
+++ src/lib/components/log-activity/log-activity.svelte
@@ -1,18 +1,33 @@
 <script lang="ts">
 	import { User } from '$lib';
+	import Slider from '../slider/slider.svelte';
 
-	console.log($User);
 	$: user = $User;
+	$: showFeedback = false;
 </script>
 
-<div class="flex flex-col rounded-lg bg-dark-600 p-6 text-dark-100">
-	<div>
-		<h2>Today you should do...</h2>
-	</div>
-	<div>
-		<p>
-			<span>{user.currentCount}</span>
-			<span>burpees</span>
-		</p>
-	</div>
+<div class="flex flex-col gap-4 rounded-lg bg-dark-600 p-6 text-dark-100">
+	{#if !showFeedback}
+		<div>
+			<h2 class="text-lg font-semibold">Today you should do...</h2>
+		</div>
+		<div>
+			<p>
+				<span>{user.currentCount}</span>
+				<span>burpees</span>
+			</p>
+		</div>
+		<div>
+			<button
+				class="focus:shadow-outline rounded bg-accent-700 px-4 py-2 font-bold text-white hover:bg-accent-500 focus:outline-none"
+				onclick={() => (showFeedback = true)}>I did them!</button
+			>
+		</div>
+	{/if}
+	{#if showFeedback}
+		<div>
+			<h2 class="text-lg font-semibold">How was it?</h2>
+			<Slider />
+		</div>
+	{/if}
 </div>
---
+++ src/lib/components/slider/slider.svelte
@@ -0,0 +1,34 @@
+<script>
+	import Range from './range.svelte';
+	let value = 42;
+	let theme = 'default';
+</script>
+
+<div class={theme === 'purple' ? 'purple-theme' : ''}>
+	<label for="basic-range" class="mx-2 my-2 block text-base font-semibold"> Range Label </label>
+	<Range on:change={(e) => (value = e.detail.value)} id="basic-slider" />
+</div>
+
+<h3 class="text-center">
+	{value}
+</h3>
+
+<h3 class="text-center">
+	Theme: {theme}
+</h3>
+
+<div class="flex justify-center">
+	<button on:click={() => (theme = 'default')} class="mx-1"> Default </button>
+	<button on:click={() => (theme = 'purple')} class="mx-1"> Purple </button>
+</div>
+
+<style>
+	:global(.purple-theme) {
+		--track-focus: #c368ff;
+		--track-highlight-bgcolor: #c368ff;
+		--track-highlight-bg: linear-gradient(90deg, #c368ff, #c965ff);
+		--thumb-holding-outline: rgba(191, 102, 251, 0.3);
+		--tooltip-bgcolor: #c368ff;
+		--tooltip-bg: linear-gradient(45deg, #c368ff, #c965ff);
+	}
+</style>
